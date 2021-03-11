d6e546abc1f2943bde6531b13820ca3538c2e0d0
Brown, Keegan <Keegan.Brown@dsa.de>
2021-03-11 12:17:00 +0100

    Minimal working Checkbutton with variable

--- duo2anki/gui.py
+++ duo2anki/gui.py
@@ -2,9 +2,33 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
+class DuoWordsGui(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._title = ttk.Label(self, text="Duolingo Words", font='-weight bold -size 10')
+        self._title.pack(side=tk.TOP)
+
+        self._chx_uncategorised = ttk.Checkbutton(self, text="Uncategorised only")
+        self._chx_uncategorised.var = tk.IntVar()                                   # type: ignore
+        self._chx_uncategorised.var.set(0)                                          # type: ignore
+        self._chx_uncategorised.configure(variable=self._chx_uncategorised.var)     # type: ignore
+        self._chx_uncategorised.pack()
+
+
+class AnkiWordsGui(tk.Frame):
+    pass
+
+
 class Duo2AnkiGui(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._duo = DuoWordsGui(self)
+        self._duo.pack(side=tk.LEFT)
+
+        self._anki = AnkiWordsGui(self)
+        self._anki.pack(side=tk.RIGHT)
 
 
 def main():
