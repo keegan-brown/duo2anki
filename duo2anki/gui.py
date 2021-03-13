e91ecec6dc67c9f69a3e6b503b45a84af6b913eb
Keegan <keeganbrown@hotmail.com>
2021-03-13 12:48:45 +0100

    Gui initial structure complete

--- duo2anki/gui.py
+++ duo2anki/gui.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from typing import Optional
 
-
 from duo2anki.model import Model, ModelTemplate
+
 
 PBIG, PSMALL = 5, 2     # padding constants
 
@@ -16,8 +16,7 @@ class DuoWordsGui(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._title = ttk.Label(self, text="Duolingo Words", font='-weight bold -size 10')
-        self._title.pack(side=tk.TOP, fill=tk.X, padx=PSMALL, pady=PSMALL)
+        ttk.Label(self, text="Duolingo Words", font='-weight bold -size 10').pack(side=tk.TOP, fill=tk.X, padx=PSMALL, pady=PSMALL)        
 
         self._chx_uncategorised = ttk.Checkbutton(self, text="Uncategorised only")
         self._chx_uncategorised.var = tk.IntVar(value=0)                            # type: ignore        
@@ -27,14 +26,19 @@ class DuoWordsGui(tk.Frame):
         sub_frame1 = tk.Frame(self)
         sub_frame1.pack(side=tk.TOP, fill=tk.X)
 
-        self._lbl_filter = tk.Label(sub_frame1, text="Filter: ")
-        self._lbl_filter.pack(side=tk.LEFT, padx=PSMALL, pady=PSMALL)
+        tk.Label(sub_frame1, text="Filter: ").pack(side=tk.LEFT, padx=PSMALL, pady=PSMALL)        
 
         self._tbx_filter = ttk.Entry(sub_frame1)
         self._tbx_filter.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=PSMALL, pady=PSMALL)
 
-        self._lbx_words = DuoWordsGui.DuoWordsListbox(self)
-        self._lbx_words.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+        sub_frame2 = tk.Frame(self)
+        sub_frame2.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+
+        self._lbx_words = DuoWordsGui.DuoWordsListbox(sub_frame2)
+        self._lbx_words.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+        scroll = ttk.Scrollbar(sub_frame2, command=self._lbx_words.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+        self._lbx_words.configure(yscrollcommand=scroll.set)
 
     def refresh(self, json_data: ModelTemplate):
         pass
@@ -47,9 +51,8 @@ class AnkiWordsGui(tk.Frame):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self._title = ttk.Label(self, text="Anki Words", font='-weight bold -size 10')
-        self._title.pack(side=tk.TOP, fill=tk.X, padx=PSMALL, pady=PSMALL)
-
+        ttk.Label(self, text="Anki Words", font='-weight bold -size 10').pack(side=tk.TOP, fill=tk.X, padx=PSMALL, pady=PSMALL)
+        
         self._chx_untranslated = ttk.Checkbutton(self, text="No translations only")
         self._chx_untranslated.var = tk.IntVar(value=0)                         # type: ignore        
         self._chx_untranslated.configure(variable=self._chx_untranslated.var)   # type: ignore
@@ -58,14 +61,20 @@ class AnkiWordsGui(tk.Frame):
         sub_frame1 = tk.Frame(self)
         sub_frame1.pack(side=tk.TOP, fill=tk.X)
 
-        self._lbl_filter = tk.Label(sub_frame1, text="Filter: ")
-        self._lbl_filter.pack(side=tk.LEFT, padx=PSMALL, pady=PSMALL)
+        tk.Label(sub_frame1, text="Filter: ").pack(side=tk.LEFT, padx=PSMALL, pady=PSMALL)
 
         self._tbx_filter = ttk.Entry(sub_frame1)
         self._tbx_filter.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=PSMALL, pady=PSMALL)
 
-        self._lbx_words = AnkiWordsGui.AnkiWordsListbox(self)
-        self._lbx_words.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+        sub_frame2 = tk.Frame(self)
+        sub_frame2.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+
+        self._lbx_words = AnkiWordsGui.AnkiWordsListbox(sub_frame2)
+        self._lbx_words.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+        scroll = ttk.Scrollbar(sub_frame2, command=self._lbx_words.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+        self._lbx_words.configure(yscrollcommand=scroll.set)
+
 
     def refresh(self, json_data: ModelTemplate):
         pass
@@ -73,35 +82,49 @@ class AnkiWordsGui(tk.Frame):
 class AnkiCardGui(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self._tags = {
-            'noun': ttk.Checkbutton(self, text='Noun'),
-            'verb': ttk.Checkbutton(self, text='Verb'),
-            'adjective': ttk.Checkbutton(self, text='Adjective'),
-        }
         
         ttk.Label(self, text="Anki Card", font='-weight bold -size 10').grid(row=0, column=0, columnspan=2, sticky=tk.W, padx=PSMALL, pady=PSMALL)
+        
         ttk.Label(self, text="Front side:").grid(row=1, column=0, sticky=tk.E, padx=PSMALL, pady=PSMALL)
-        ttk.Label(self, text="Back side:").grid(row=2, column=0, sticky=tk.E, padx=PSMALL, pady=PSMALL)
-        ttk.Label(self, text="Tags:").grid(row=3, column=0, sticky=tk.E, padx=PSMALL, pady=PSMALL)
-        ttk.Separator(self, orient=tk.HORIZONTAL).grid(row=3+len(self._tags), column=0, columnspan=2, sticky=tk.EW, padx=PSMALL, pady=PSMALL)
-        ttk.Label(self, text="Linked Duolingo words:").grid(row=4+len(self._tags), column=0, columnspan=2, sticky=tk.W, padx=PSMALL, pady=PSMALL)
 
         self._tbx_front = ttk.Entry(self)
         self._tbx_front.grid(row=1, column=1, padx=PSMALL, pady=PSMALL)
 
+        ttk.Label(self, text="Back side:").grid(row=2, column=0, sticky=tk.E, padx=PSMALL, pady=PSMALL)
+
         self._tbx_back = ttk.Entry(self)
         self._tbx_back.grid(row=2, column=1, padx=PSMALL, pady=PSMALL)
 
-        for i, check in enumerate(self._tags.values()):
-            check.var = tk.IntVar(value=0)          # type: ignore -> dynamic attribute
-            check.configure(variable=check.var)     # type: ignore -> dynamic attribute
-            check.grid(row=3 + i, column=1, sticky=tk.W, padx=PSMALL, pady=PSMALL)
+        ttk.Label(self, text="Tags:").grid(row=3, column=0, sticky=tk.NE, padx=PSMALL, pady=PSMALL)
 
-        self._lbx_words = tk.Listbox(self)
-        self._lbx_words.grid(row=5+len(self._tags), column=0, columnspan=2, sticky=tk.NSEW, padx=PSMALL, pady=PSMALL)
+        sub_frame1 = tk.Frame(self)
+        sub_frame1.grid(row=3, column=1, sticky=tk.NSEW, padx=PSMALL, pady=PSMALL)
 
-        self.rowconfigure(index=5+len(self._tags), weight=1)
+        self._tags = {
+            'noun': ttk.Checkbutton(sub_frame1, text='Noun'),
+            'verb': ttk.Checkbutton(sub_frame1, text='Verb'),
+            'adjective': ttk.Checkbutton(sub_frame1, text='Adjective'),
+        }
+
+        for cbx in self._tags.values():
+            cbx.var = tk.IntVar(value=0)    # type: ignore -> dynamic attribute
+            cbx.configure(variable=cbx.var) # type: ignore -> dynamic attribute
+            cbx.pack(side=tk.TOP, fill=tk.X, padx=PSMALL//2, pady=PSMALL//2)
+
+        ttk.Separator(self, orient=tk.HORIZONTAL).grid(row=4, column=0, columnspan=2, sticky=tk.EW, padx=PSMALL, pady=PSMALL)
+
+        ttk.Label(self, text="Linked Duolingo words:").grid(row=5, column=0, columnspan=2, sticky=tk.W, padx=PSMALL, pady=PSMALL)
+
+        sub_frame2 = tk.Frame(self)
+        sub_frame2.grid(row=6, column=0, columnspan=2, sticky=tk.NSEW, padx=PSMALL, pady=PSMALL)
+
+        self._lbx_words = tk.Listbox(sub_frame2)
+        self._lbx_words.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
+        scroll = ttk.Scrollbar(sub_frame2, command=self._lbx_words.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+        self._lbx_words.configure(yscrollcommand=scroll.set)
+
+        self.rowconfigure(index=6, weight=1)
         
 
 class Duo2AnkiGui(tk.Frame):
@@ -109,6 +132,10 @@ class Duo2AnkiGui(tk.Frame):
         super().__init__(*args, **kwargs)
 
         self._model: Optional[Model] = None 
+        self._setup_ui()
+        
+
+    def _setup_ui(self):
 
         self._duo = DuoWordsGui(self)
         self._duo.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=PSMALL, pady=PSMALL)
@@ -122,6 +149,13 @@ class Duo2AnkiGui(tk.Frame):
 
         self._anki_card = AnkiCardGui(self)
         self._anki_card.pack(side=tk.LEFT, fill=tk.BOTH, padx=PSMALL, pady=PSMALL)
+
+        menu = tk.Menu(self)
+        menu_file = tk.Menu(menu, tearoff=0)
+        menu_file.add_command(label="Import Duolingo words ...")
+        menu_file.add_command(label="Export Anki .csv ...")
+        menu.add_cascade(label='File', menu=menu_file)
+        self.master.configure(menu=menu) # type: ignore -> menu exists in root
 
     def refresh(self):
         json_data = self._model.get_json()
